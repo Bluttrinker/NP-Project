052245708ceacc543c44d8cefaa26263488f9550
Immo Stanke <s9imstan@stud.uni-saarland.de>
2012-07-09 13:10:25 +0200

    created project skeleton

---
+++ NebenlaeufigeProgrammierung/src/correction/Assistant.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+/**
+ *
+ * @author bluttrinker
+ */
+public class Assistant implements Runnable {
+
+    private int assignedExercise;
+
+    public Assistant(int assignedExercise) {
+        this.assignedExercise = assignedExercise;
+    }
+    
+    
+    
+    @Override
+    public void run() {
+        //TODO: Implement this method
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ NebenlaeufigeProgrammierung/src/correction/ExamStack.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Project;
+
+/**
+ *
+ * @author bluttrinker
+ */
+public class ExamStack {
+    
+   public void assiPush(Exam exam){
+      // TODO: Implement this method
+       throw new UnsupportedOperationException("not supported yet");
+   } 
+   
+   public Exam assiPop(){
+       // TODO: Implement this method
+       throw new UnsupportedOperationException("not supported yet");
+       
+   }
+    
+   public void profPush(Exam exam){
+       // TODO: Implement this method
+       throw new UnsupportedOperationException("not supported yet");
+   }
+   
+   public Exam profPull(){
+       // TODO: Implement this method
+       throw new UnsupportedOperationException("not supported yet");
+         
+   }
+   
+   private void distribute(){
+       // TODO: Implement this method
+       throw new UnsupportedOperationException("not supported yet");
+   }
+   
+}
